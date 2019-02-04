dbbd09c8228cd0fc09ce155fa9d3bf8c48f4f349
Juan Manuel Servera <jmservera@outlook.com>
2019-02-04 16:53:28 +0100

    fix sh and add --force-yes to install

--- install_TPM.sh
+++ install_TPM.sh
@@ -1,11 +1,11 @@
-#!bin/bash
+#!/bin/sh
 
 mkdir tpm_sim
 cd tpm_sim
 curl -O https://netcologne.dl.sourceforge.net/project/ibmswtpm2/ibmtpm1332.tar.gz
 tar -xvzf ibmtmp1332.tar.gz
 cd src
-apt-get update && sudo apt-get install make gcc libssl-dev tss2
+apt-get update && sudo apt-get install --force-yes make gcc libssl-dev tss2
 make
 cp tpm_server /sbin
 cd ..
