c1f0f30dde59f99820d58e8a56610dd32e98fe1e
Ubuntu <jmservera@jmserveraiotubuntu18.jxhn0ugu2jgedbdbgxwpwv3jrf.fx.internal.cloudapp.net>
2019-02-04 21:57:26 +0000

    first working script

--- install_TPM.sh
+++ install_TPM.sh
@@ -2,18 +2,20 @@
 
 mkdir tpm_sim
 cd tpm_sim
+echo $(pwd)
 curl -O https://netcologne.dl.sourceforge.net/project/ibmswtpm2/ibmtpm1332.tar.gz
-tar -xvzf ibmtmp1332.tar.gz
-cd src
+ls
+tar -xvzf ibmtpm1332.tar.gz
 apt-get update && sudo apt-get install -y make gcc libssl-dev tss2
+cd src
 make
 cp tpm_server /sbin
-cd ..
+cd ../..
 cp tpm_server.service /etc/systemd/system/
 chmod 777 /etc/systemd/system/tpm_server.service
 systemctl daemon-reload
 systemctl enable tpm_server.service
-systemctl start tpm_servers
+systemctl start tpm_server
 tsspowerup
 tssstartup
 TPM_INTERFACE_TYPE=socsim time tsscreateprimary -hi o -st -rsa
