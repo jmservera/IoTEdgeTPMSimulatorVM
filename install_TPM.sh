60eb03ed7865008d9c76ecb23fce69131374a041
Juan Manuel Servera <jmservera@outlook.com>
2019-02-04 17:31:42 +0100

    correct name

--- install_TPM.sh
+++ install_TPM.sh
@@ -3,7 +3,7 @@
 mkdir tpm_sim
 cd tpm_sim
 curl -O https://netcologne.dl.sourceforge.net/project/ibmswtpm2/ibmtpm1332.tar.gz
-tar -xvzf ibmtmp1332.tar.gz
+tar -xvzf ibmtpm1332.tar.gz
 cd src
 apt-get update && sudo apt-get install -y make gcc libssl-dev tss2
 make
