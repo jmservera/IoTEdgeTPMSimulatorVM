9c213a73b20740b4d739a8076aec676966f9e03d
Juan Manuel Servera <jmservera@outlook.com>
2019-02-04 16:41:20 +0100

    add simple sh

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # IoTEdgeTPMSimulatorVM
+
 Using a software TPM for simulating it in an Azure VM or on a device without a hardware TPM.
+
+## Installing the TPM Simulator inside an ubuntu 18 VM
+
+copy install_TPM.sh and tpm_server.service in the machine
+run install_TPM.sh with sudo
---
+++ install_TPM.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+mkdir tpm_sim
+cd tpm_sim
+curl -O https://netcologne.dl.sourceforge.net/project/ibmswtpm2/ibmtpm1332.tar.gz
+tar -xvzf ibmtmp1332.tar.gz
+cd src
+apt-get update && sudo apt-get install make gcc libssl-dev tss2
+make
+cp tpm_server /sbin
+cd ..
+cp tpm_server.service /etc/systemd/system/
+chmod 777 /etc/systemd/system/tpm_server.service
+systemctl daemon-reload
+systemctl enable tpm_server.service
+systemctl start tpm_servers
+tsspowerup
+tssstartup
+TPM_INTERFACE_TYPE=socsim time tsscreateprimary -hi o -st -rsa
+
+
